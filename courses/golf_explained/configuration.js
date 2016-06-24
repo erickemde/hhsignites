7d1f9e4890b52e17e1e6962eb5c212e9b3730638
unknown <FPM8@D314510.cdc.gov>
2016-06-24 13:25:42 -0400

    Initial Commit

---
+++ courses/golf_explained/configuration.js
@@ -0,0 +1,6 @@
+DispatchRoot='https://cloud.scorm.com/ScormEngineInterface/dispatch/';
+ContentAPI='https://cloud.scorm.com/ScormEngineInterface/dispatch//DispatchHost.html';
+ContentURL='https://cloud.scorm.com/EngineWebServices/api?method=rustici.dispatch.launch&dispatchid=a15eea42-bc1c-405e-be2c-9a3cd13010ae&launchsecret=uVOflM2vH56QaIhJUEZuGkmjQBrQMCBVs8pMKQOG&learnerid=LEARNER_ID&fname=LEARNER_FNAME&lname=LEARNER_LNAME&referringurl=REFERRING_URL&pipeurl=PIPE_URL&redirecturl=REDIRECT_URL&cssurl=CSS_URL_REGISTRATION_ARGUMENT';
+DefaultCssUrl='https://cloud.scorm.com/sc/css/cloudPlayer/cloudstyles.css';
+DispatchVersion=1;
+TcapiEndpoint='https://cloud.scorm.com/ScormEngineInterface/TCAPI/0D88VTK6PF';
